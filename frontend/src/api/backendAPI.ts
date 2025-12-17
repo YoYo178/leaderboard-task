41bd1b62a6bf5159e785d7fe331389150aeeb8e8
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-12-17 19:10:07 +0530

    Fix URL identifier duplication

--- frontend/src/api/backendAPI.ts
+++ frontend/src/api/backendAPI.ts
@@ -5,5 +5,5 @@ import { BACKEND_URL } from '../config/backendConfig';
 const isProduction = import.meta.env.PROD;
 
 export const API = axios.create({
-    baseURL: BACKEND_URL + `${isProduction ? '/notes-app/api' : '/api'}`,
+    baseURL: BACKEND_URL + `${isProduction ? '/notes-app' : ''}`,
 });
